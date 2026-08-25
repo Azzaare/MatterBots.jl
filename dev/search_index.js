6c02fa9d228f323e555db73cda4959329cb3ffe6
Documenter.jl <documenter@juliadocs.github.io>
2026-08-25 07:52:39 +0000

    build based on 3f96a96

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MatterBot","category":"page"},{"location":"#MatterBot","page":"Home","title":"MatterBot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatterBot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MatterBot]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MatterBots","category":"page"},{"location":"#MatterBots","page":"Home","title":"MatterBots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatterBots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MatterBots]","category":"page"}]
 }
